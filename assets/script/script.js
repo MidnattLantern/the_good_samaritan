19f4246f1a3ac7fa8b974db741358fe6af0bb954
Alma <isakvent@outlook.com>
2023-10-12 15:28:57 +0200

    made an outline for story progression

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,36 @@
-let textboxCount = 0;
-console.log("textboxCount:", textboxCount);
+// Integer decides what text should appear next
+let textboxId = 0;
+
+let textContent = "sample text";
 
 function progressStory() {
-    textboxCount += 1
-    console.log("textboxCount:", textboxCount)
+    textboxId += 1
+    console.log("textboxId:", textboxId);
+    updateTextboxId(textboxId);
+    console.log("textContent:", textContent);
+
+    // Appends integer for textboxId
     let appendText = document.getElementById("textbox");
     let newText = document.createElement("li");
-    newText.innerHTML = "new text";
+    newText.innerHTML = textContent;
     appendText.appendChild(newText);
 }
+
+// Text content changes for next append
+function updateTextboxId(x) {
+    if (x == 1) {
+        textContent = "first text"
+    } else if (x == 2) {
+        textContent = "second text"
+    } else if (x == 3) {
+        textContent = "third text"
+    } else if (x == 4) {
+        textContent = "fourth text"
+    } else if (x == 5) {
+        textContent = "fifth text"
+    }
+}
+
+// On launch
+console.log("textboxId:", textboxId);
+console.log("textContent:", textContent);
